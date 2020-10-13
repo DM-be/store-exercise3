35dc8f153b8aeb2428a969f83acdb46dbd3ff1c7
DM-be <dennis.morent@student.howest.be>
2020-10-13 20:27:34 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ import {
   selector: "my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
   public completedTodos$: Observable<Todo[]>;
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter,  Input,  OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter,  Input,  OnInit, Output } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { AppState } from '../store/app.state';
@@ -8,7 +8,8 @@ import { CompleteTodoAction, DeleteTodoAction } from '../store/todo.action';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TodoComponent implements OnInit {
 
